69a5cca3d90177b494d01083f594dda7ed57cd2f
out06 <out.06@outlook.com>
2022-06-29 16:21:09 +0800

    add file read

--- Util/Tool.go
+++ Util/Tool.go
@@ -1,7 +1,31 @@
 package Util
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
 
 func Hi() {
 	fmt.Println("hi")
+}
+
+func ReadFile() ([]string){
+	fi, _ := os.Open("./example/my.log")
+
+	defer fi.Close()
+
+	var lines []string
+	br := bufio.NewReader(fi)
+	for {
+		a, _, c := br.ReadLine()
+		if c == io.EOF {
+			break
+		}
+		lines = append(lines,string(a))
+		//fmt.Println("M:"+string(a))
+	}
+
+	return lines
 }
